a56357e15fc3049fcbb5ac9a62b9513dd0d0abc1
Aivaras <stasiulis.aivaras@gmail.com>
2020-08-20 02:57:42 +0300

    Updated router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Auth/Login.vue";
 import Register from "../views/Auth/Register.vue";
+import Dashboard from "../views/Dashboard.vue";
+import AdminPage from "../views/Admin/AdminPage.vue";
+import AdminDashboard from "../views/Admin/AdminDashboard.vue";
+import AdminUserAdd from "../views/Admin/AdminUserAdd.vue";
+import AdminUserEdit from "../views/Admin/AdminUserEdit.vue";
 
 Vue.use(VueRouter);
 
@@ -16,12 +21,60 @@ const routes = [
     path: "/login",
     name: "Login",
     props: true,
-    component: Login
+    component: Login,
+    meta: {
+      requiredAuthorization: false
+    }
   },
   {
     path: "/register",
     name: "Register",
-    component: Register
+    component: Register,
+    meta: {
+      requiredAuthorization: false
+    }
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta: {
+      requiredAuthorization: true
+    }
+  },
+  {
+    path: "/admin",
+    name: "AdminPage",
+    component: AdminPage,
+    meta: {
+      requiredAuthorization: true
+    },
+    children: [
+      {
+        path: "dashboard",
+        name: "AdminDashboard",
+        component: AdminDashboard,
+        meta: {
+          requiredAuthorization: true
+        }
+      },
+      {
+        path: "user/add",
+        name: "AdminUserAdd",
+        component: AdminUserAdd,
+        meta: {
+          requiredAuthorization: true
+        }
+      },
+      {
+        path: "user/:id/edit",
+        name: "AdminUserEdit",
+        component: AdminUserEdit,
+        meta: {
+          requiredAuthorization: true
+        }
+      }
+    ]
   }
 ];
 
